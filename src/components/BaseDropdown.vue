406c23643675e82ac344f5a6480bf71c6e83227e
unknown <lithos.contact@gmail.com>
2022-07-17 10:44:54 +0200

    First commit

---
+++ src/components/BaseDropdown.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-btn-dropdown flat class="text-capitalize" :label="label">
+    <div class="row flex justify-between q-pa-md">
+      <div class="column">
+        <div class="text-h7 q-my-auto">{{ label }}</div>
+      </div>
+      <div class="column">
+        <div class="text-right">
+          <q-btn fab-mini small icon="close" flat />
+        </div>
+      </div>
+    </div>
+
+    <q-list bordered>
+      <q-item clickable v-ripple v-for="(item, i) of items" :key="i">
+        <q-item-section avatar>
+          <q-img :src="item.image" />
+        </q-item-section>
+
+        <q-item-section>{{ item.link }}</q-item-section>
+      </q-item>
+    </q-list>
+  </q-btn-dropdown>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: "Dropdown",
+  },
+  items: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
+
+<style></style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> Allegro </q-toolbar-title>
+        <q-btn color="secondary">Create</q-btn>
+        <BaseDropdown
+          label="Workspaces"
+          :items="workspaces"
+          class="q-mx-sm"
+        ></BaseDropdown>
+        <BaseDropdown
+          label="Recent"
+          :items="workspaces"
+          class="q-mx-sm"
+        ></BaseDropdown>
+
+        <q-input
+          dark
+          dense
+          standout
+          v-model="text"
+          label="Search"
+          class="q-mx-sm"
+        >
+          <template v-slot:append>
+            <q-icon v-if="text === ''" name="search" />
+            <q-icon
+              v-else
+              name="clear"
+              class="cursor-pointer"
+              @click="text = ''"
+            />
+          </template>
+        </q-input>
+
+        <div>v0.0.1</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import BaseDropdown from "../components/BaseDropdown.vue";
+
+const workspaces = [
+  {
+    link: "Workspace 1",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    link: "Workspace 2",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    link: "Workspace 3",
+    image: "https://picsum.photos/200/300",
+  },
+];
+</script>
